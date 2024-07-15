7b4b89afafc140702fc896a812712c489f6205b3
Dylan Alvandi <g.dylanalvandi@gmail.com>
2024-07-15 02:33:40 +0200

    feat: Rename CLI tool

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use clap::Parser;
 
 /// Echo CLI
 #[derive(Parser, Debug)]
-#[command(name = "echo_cli", version = "1.0", author = "Sudohaxe", about = "Echoes the input provided by the user")]
+#[command(name = "sudohaxe_cli", version = "1.0", author = "Sudohaxe", about = "Echoes the input provided by the user")]
 struct Args {
     input: String,
 }
 
 fn main() {
     let args = Args::parse();
-    println!("{}", args.input);
+
+    println!("Hello, {}", args.input);
 }
